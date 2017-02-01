3d98044ecaf009051134b4b099ce178f812b8d58
KateHarris142 <kateharris142@gmail.com>
2017-02-01 09:42:26 +0000

    corrected plot and added text output

--- ShinyAssignment/Diamonds/server.R
+++ ShinyAssignment/Diamonds/server.R
@@ -8,28 +8,101 @@
 #
 
 library(shiny)
-diam <- read.csv("Diamonds.csv")
+
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output,session) {
+    
+    data <- diamonds
+    
    output$group1ui <- renderUI({
-       switch(input$InFamily,
-              "Carat" = 
-       
-       
-       
-       
+        switch(input$factor,
+              "Carat" = textInput("caratrange","What is the Carat of your diamond? (between 0.2 and 5.01)",value=3.0),
+              "Cut" = radioButtons("cut","What is the cut of your diamond?",
+                                   choices = c("Fair","Good","Very Good","Premium","Ideal")),
+              "Colour" = radioButtons("color","What is the colour of your diamond?",
+                                      choices = c("D","E","F","G","H","I","J")),
+              "Clarity" = radioButtons("clarity","What is the clarity of your diamond?",
+                                       choices = c("IF","VS1","VS2","VVS1","VVS2"))
+         )
    })
-    
-    
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+   
+ 
+  output$FullPlot <- renderPlot({
+      if(input$factor == "Carat"){
+          dat <- as.data.frame(diamonds[,c("carat","price"),])
+          subcarat <- dat[which(dat$carat==input$carat),]
+          maxV <- max(subcarat$price)
+          minV <- max(subcarat$price)
+          meanV <- mean(subcarat$price)
+          plot(dat[,1],dat[,2],
+               ylab="Singapore dollars $",xlab="carats",
+               xlim=c(0,5.5),ylim=c(300,19000))
+      }
+      if(input$factor == "Cut"){
+          dat <- as.data.frame(diamonds[,c("cut","price"),])
+          subcut <- dat[which(dat$cut==input$cut),]
+          maxV <- max(subcut$price)
+          minV <- max(subcut$price)
+          meanV <- mean(subcut$price)
+          plot(dat[,1],dat[,2],
+               ylab="Singapore dollars $",xlab="cut")
+      }
+      if(input$factor == "Colour"){
+          dat <- as.data.frame(diamonds[,c("color","price"),])
+          subcol <- dat[which(dat$color==input$color),]
+          maxV <- max(subcol$price)
+          minV <- max(subcol$price)
+          meanV <- mean(subcol$price)
+          plot(dat[,1],dat[,2],
+               ylab="Singapore dollars $",xlab="colour")
+      }
+      if(input$factor == "Clarity"){
+          dat <- as.data.frame(diamonds[,c("clarity","price"),])
+          subclarity <- dat[which(dat$clarity==input$clarity),]
+          maxV <- max(subclarity$price)
+          minV <- max(subclarity$price)
+          meanV <- mean(subclarity$price)
+          plot(dat[,1],dat[,2],
+               ylab="Singapore dollars $",xlab="clarity")
+      }
+      
+      
+  }) 
+  
+  output$value <- renderText({
+      if(input$factor == "Carat"){
+          dat <- as.data.frame(diamonds[,c("carat","price"),])
+          subcarat <- dat[which(dat$carat>(as.numeric(input$caratrange)-0.5) & dat$carat<(as.numeric(input$caratrange)+0.5)),] 
+          maxV <- max(subcarat$price)   
+          minV <- min(subcarat$price) 
+          meanV <- mean(subcarat$price) 
+      }  
+      if(input$factor == "Cut"){
+          dat <- as.data.frame(diamonds[,c("cut","price"),])
+          subcut <- dat[which(dat$cut==input$cut),]
+          maxV <- max(subcut$price)
+          minV <- min(subcut$price)
+          meanV <- mean(subcut$price)
+      }
+      if(input$factor == "Colour"){
+          dat <- as.data.frame(diamonds[,c("color","price"),])
+          subcol <- dat[which(dat$color==input$color),]
+          maxV <- max(subcol$price)
+          minV <- min(subcol$price)
+          meanV <- mean(subcol$price)
+      }
+      if(input$factor == "Clarity"){
+          dat <- as.data.frame(diamonds[,c("clarity","price"),])
+          subclarity <- dat[which(dat$clarity==input$clarity),]
+          maxV <- max(subclarity$price)
+          minV <- min(subclarity$price)
+          meanV <- mean(subclarity$price)
+      }
+      
+
+      paste0("The range of prices for your dimaond is $", minV, "-$", maxV,". The mean price is $", round(meanV,0),".")
   })
+  
+  
   
 })
--- ShinyAssignment/Diamonds/ui.R
+++ ShinyAssignment/Diamonds/ui.R
@@ -15,11 +15,20 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Predict the price of your diamond"),
   
+  helpText('This App uses the R diamonds dataset to look at the ranges of carat, colour, clarity and cut of diamonds.
+           To use, select what you know about your diamond, for exxample the carat. 
+           Below will appear a new input where you can add/select the availble options.
+           The plot will show all the spread of all the data for that variable, while the text will give you the 
+           range and mean for your selected input.
+           From this you can see how expensive (and the range) you would expect your diamond to be based on the 
+           information you have, and also the ranges for different diamonds.'),
+  
+  
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
         radioButtons("factor",label="What do you know about your diamond?",
-                     choices=c("Carat","colour","clarity")),
+                     choices=c("Carat","Colour","Clarity","Cut")),
         uiOutput("group1ui")
     ),
     
